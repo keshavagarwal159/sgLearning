ccf53ed4a79721c2f0e5b64037a398bb20ff6db3
keshavagarwal159 <keshavagarwal159@gmail.com>
2026-04-06 12:56:19 +0530

    add new print

--- src/main/java/com/sglearning/kubeproject/KubeprojectApplication.java
+++ src/main/java/com/sglearning/kubeproject/KubeprojectApplication.java
@@ -8,6 +8,7 @@ public class KubeprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KubeprojectApplication.class, args);
+		System.out.println("Hello, Kubernetes!");
 	}
 
 }
