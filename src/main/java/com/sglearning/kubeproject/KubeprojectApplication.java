7efddcd92ecbcdc5a13092019cc3ff7fd2809f53
keshavagarwal159 <keshavagarwal159@gmail.com>
2026-04-06 12:14:13 +0530

    Initial commit - kubeproject

---
+++ src/main/java/com/sglearning/kubeproject/KubeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sglearning.kubeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sglearning/kubeproject/KubeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sglearning.kubeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
